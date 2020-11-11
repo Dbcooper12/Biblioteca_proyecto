6f749fd050299f0b2f418f02ab7f2e439a5bf612
Dbcooper12 <66926749+Dbcooper12@users.noreply.github.com>
2020-11-11 14:55:12 -0500

    clase devolucion
    clase devolucion

--- src/capaNegocio/clsDevolucion.java
+++ src/capaNegocio/clsDevolucion.java
@@ -1,15 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package capaNegocio;
+
+import capaDatos.clsJDBC;
+import java.sql.ResultSet;
+import java.util.Date;
 
 /**
  *
  * @author manue
  */
 public class clsDevolucion {
-    //hola
-    
+
+    clsJDBC objConectar = new clsJDBC();
+    String strSQL;
+    ResultSet rs = null;
+
+    public Integer generarCodigoDevolucion() throws Exception {
+        strSQL = "SELECT COALESCE(max(codigodevolucion),0)+1 as codigodevolucion from devolucion";
+        try {
+            rs = objConectar.consultarBD(strSQL);
+            while (rs.next()) {
+                return rs.getInt("codigodevolucion");
+            }
+        } catch (Exception e) {
+            throw new Exception("Error al generar código de Devolucion");
+        }
+        return 0;
+    }
+
+    public ResultSet listarDevolucion() throws Exception {
+        strSQL = "SELECT * FROM devolucion";
+        try {
+            rs = objConectar.consultarBD(strSQL);
+            return rs;
+        } catch (Exception e) {
+            throw new Exception("Error al buscar devolucion");
+        }
+    }
+
+    public void registrar(Integer codigodevolucion, Integer numeroPrestamo, String nombreLibro, String fechaDevolucion, String dnialumno, String estado) throws Exception {
+        strSQL = "insert into devolucion values("+codigodevolucion+"," +  numeroPrestamo +",'" + nombreLibro +"','" + dnialumno + "','"+estado+"','"+fechaDevolucion+"')";
+      
+        try {
+            objConectar.ejecutarBD(strSQL);
+        } catch (Exception e) {
+            throw new Exception("Error al registrar devolucion" + e.getMessage());
+        }
+
+    }
+
 }
