043adc1f08b10f598759cc2c50958f693ccb23a6
Dbcooper12 <66926749+Dbcooper12@users.noreply.github.com>
2020-11-11 14:56:44 -0500

    clase prestamo
    clase prestamo

--- src/capaNegocio/clsPrestamo.java
+++ src/capaNegocio/clsPrestamo.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package capaNegocio;
+
+import capaDatos.clsJDBC;
+import java.sql.ResultSet;
 
 /**
  *
  * @author manue
  */
 public class clsPrestamo {
-    
+
+    clsJDBC objConectar = new clsJDBC();
+    String strSQL;
+    ResultSet rs = null;
+
+    public ResultSet listarPrestamo() throws Exception {
+        strSQL = "SELECT * FROM prestamo;";
+        try {
+            rs = objConectar.consultarBD(strSQL);
+            return rs;
+        } catch (Exception e) {
+            throw new Exception("Error al buscar Prestamo");
+        }
+    }
 }
