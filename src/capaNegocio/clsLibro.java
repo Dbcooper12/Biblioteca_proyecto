911960d02cf567623e32f456797167537c469a56
monicasz <70986045+monicasz@users.noreply.github.com>
2020-11-11 13:41:29 -0500

    clase libro - monica
    modificado libro

--- src/capaNegocio/clsLibro.java
+++ src/capaNegocio/clsLibro.java
@@ -21,13 +21,12 @@ public class clsLibro {
     
     
     
-    public void Registrar_Libro(Integer cod,String p_nombre, String p_autor, String p_pais, java.util.Date p_fecha, int p_codigoEditorial,int p_codigoEspecialidad, Boolean estado) throws Exception {
-        strSQL = "insert into libro values (" + cod + ", '" + p_nombre + "', '" + p_autor + "', "
-                + "'" + p_pais + "', '" + p_fecha + "'," + p_codigoEditorial+ ", " + p_codigoEspecialidad   + ", " + estado +")";
+    public void Registrar_Libro(Integer cod,String p_nombre, String p_autor, String p_pais, java.util.Date p_fecha,int p_codigoEditorial,int p_codigoEspecialidad) throws Exception {
+        strSQL = "insert into libro values (" + cod + ", '" + p_nombre + "', '" + p_autor + "', '" + p_pais + "', '" + p_fecha + "','DISPONIBLE'," + p_codigoEditorial+ ", " + p_codigoEspecialidad   + ")";
         try {
             objConectar.ejecutarBD(strSQL);
         } catch (Exception e) {
-            throw new Exception(e.getMessage());
+           throw new Exception("Error al registrar libro");
         }
     }
     
@@ -66,11 +65,11 @@ public class clsLibro {
             rs=objConectar.consultarBD(strSQL);
             return rs;
         } catch (Exception e) {
-            throw new Exception(e.getMessage() + ": Error al consultar productos.") ;
+            throw new Exception(e.getMessage() + ": Error al consultar libro.") ;
         }
     }
         public ResultSet Listar() throws Exception {
-        strSQL = "select l.*,e.nombreeditorial,es.nombreespecialidad from libro l inner join editorial e on e.codigoeditorial = l.codigoeditorial inner join especialidad es on es.codigoespecialidad = l.codigoespecialidad" ;
+        strSQL = "select l.*,e.nombreeditorial,es.nombreespecialidad from libro l inner join editorial e on e.codigoeditorial = l.codigoeditorial inner join especialidad es on es.codigoespecialidad = l.codigoespecialidad order by codigolibro" ;
                   
         try {
             rs = objConectar.consultarBD(strSQL);
@@ -79,8 +78,18 @@ public class clsLibro {
             throw new Exception(e.getMessage() + "error al consultar lista" + e.getLocalizedMessage());
         }
     }
-    public void Modificar(int codLibro, String p_nombre, String p_autor, String p_pais, java.util.Date p_fecha,int p_codigoEditorial,int p_codigoEspecialidad, Boolean estado) throws Exception {
-        strSQL = "update libro set nombre = '" + p_nombre + "', autor = '" + p_autor + "',pais = '" + p_pais + "',fecha = '" + p_fecha + "',codigoeditorial = " + p_codigoEditorial + ", codigoespecialidad = " + p_codigoEspecialidad + ", estado=" + estado +" where codigolibro = " + codLibro;
+        public ResultSet ListarLibro() throws Exception {
+        strSQL = "select l.codigolibro, l.nombre,l.autor, l.estado,e.nombreeditorial,es.nombreespecialidad from libro l inner join editorial e on l.codigoeditorial = e.codigoeditorial inner join especialidad es on l.codigoespecialidad = es.codigoespecialidad" ;
+                  
+        try {
+            rs = objConectar.consultarBD(strSQL);
+            return rs;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage() + "error al consultar lista" + e.getLocalizedMessage());
+        }
+    }
+    public void Modificar(int codLibro, String p_nombre, String p_autor, String p_pais, java.util.Date p_fecha,int p_codigoEditorial,int p_codigoEspecialidad) throws Exception {
+        strSQL = "update libro set nombre = '" + p_nombre + "', autor = '" + p_autor + "',pais = '" + p_pais + "',fecha = '" + p_fecha + "',codigoeditorial = " + p_codigoEditorial + ", codigoespecialidad = " + p_codigoEspecialidad + " where codigolibro = " + codLibro;
         try {
             objConectar.ejecutarBD(strSQL);
         } catch (Exception e) {
@@ -89,23 +98,35 @@ public class clsLibro {
 
     }
     
-        public void DarBaja(int cod) throws Exception {
-        strSQL = "update libro set estado=false where codigoLibro=" + cod;
+    public ResultSet buscarlibropornombre(String nombre) throws Exception{
+        strSQL="select l.codigolibro, l.nombre,l.autor,l.estado,e.nombreeditorial,es.nombreespecialidad from libro l inner join editorial e on l.codigoeditorial = e.codigoeditorial inner join especialidad es on l.codigoespecialidad = es.codigoespecialidad where nombre like '" + nombre + "%' order by codigolibro "; 
         try {
-            objConectar.ejecutarBD(strSQL);
+            rs=objConectar.consultarBD(strSQL);
+            return rs;
         } catch (Exception e) {
-            throw new Exception("Error al dar de baja libro" + e.getMessage());
+            throw new Exception(e.getMessage() + ": Error al consultar libro.") ;
         }
-    }
-
-    public void Eliminar(int cod) throws Exception {
-
-        strSQL = "delete from libro where codigoLibro = " + cod;
+    
+}
+     public ResultSet buscarlibroporDNI (String dni) throws Exception{
+        strSQL="select al.dniestudiante,al.ap_paterno,al.ap_materno,al.nombre,r.codigoreserva, r.fechareserva,l.nombre,l.autor,r.estado from reserva r inner join libro l on r.codigolibro = l.codigolibro inner join alumno al on r.dnialumno = al.dniestudiante where r.dnialumno='" + dni + "'"; 
         try {
-            objConectar.ejecutarBD(strSQL);
+            rs=objConectar.consultarBD(strSQL);
+            return rs;
         } catch (Exception e) {
-            throw new Exception("Error al eliminar Libro" + e.getMessage());
+            throw new Exception(e.getMessage() + ": Error al consultar libro!.") ;
         }
-    }  
+    
+}
+      public ResultSet buscarlibroporEstado (String estado) throws Exception{
+        strSQL="select l.codigolibro, l.nombre,l.autor,e.nombreeditorial,es.nombreespecialidad, l.estado from libro l inner join editorial e on l.codigoeditorial = e.codigoeditorial inner join especialidad es on l.codigoespecialidad = es.codigoespecialidad where l.estado='" + estado + "' order by codigolibro"; 
+        try {
+            rs=objConectar.consultarBD(strSQL);
+            return rs;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage() + ": Error al consultar libro.!") ;
+        }
+    
+}
 }
 
